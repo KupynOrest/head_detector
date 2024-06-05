9d41e10ca52d075c68a08b75afc2d8259df184a8
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2024-06-05 12:12:59 +0300

    SegformerB5Heads

--- yolo_head_training/yolo_head/segformer_heads.py
+++ yolo_head_training/yolo_head/segformer_heads.py
@@ -1,11 +1,20 @@
+import copy
+from pathlib import Path
+from typing import Union
+
+from omegaconf import DictConfig
 from super_gradients.common.factories.detection_modules_factory import DetectionModulesFactory
 from super_gradients.common.registry import register_model
 from super_gradients.training.models.segmentation_models.segformer import MiTBackBone
 from torch import nn
+from super_gradients.common.registry import register_model
+from super_gradients.training.models import get_arch_params
+from super_gradients.training.utils import HpmStruct, get_param
+from super_gradients.training.models import SgModule
 
 
 @register_model()
-class SegFormerYoloHeads(nn.Module):
+class SegFormerYoloHeads(SgModule):
     def __init__(
         self,
         backbone,
@@ -35,3 +44,18 @@ class SegFormerYoloHeads(nn.Module):
         x = self._backbone(x)
         x = x[1:]
         return self.heads(x)
+
+
+@register_model()
+class SegformerB5Heads(SegFormerYoloHeads):
+    def __init__(self, arch_params: Union[HpmStruct, DictConfig]):
+        recipes_dir_path = Path(__file__).parent.parent / "configs"
+
+        default_arch_params = get_arch_params("segformer_b5_yolo", recipes_dir_path=str(recipes_dir_path))
+        merged_arch_params = HpmStruct(**copy.deepcopy(default_arch_params))
+        merged_arch_params.override(**arch_params.to_dict())
+        super().__init__(
+            backbone=merged_arch_params.backbone,
+            heads=merged_arch_params.heads,
+            num_classes=get_param(merged_arch_params, "num_classes", None),
+        )
